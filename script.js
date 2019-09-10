f57886ec8ca59a0db5cfb7b9c877e9c1f8687430
Alina Dicoiu <alina.dicoiu@ebs.msft>
2019-09-10 16:10:36 +0300

    added loto ticket grid

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 $("document").ready(function () {
-    var randomNumbers=[];
-    var userNumbers=[];
-    
-    $("#extractbutton").on("click", function (evt1){
-        var ct=0;
+    createGrid(7);
+    var randomNumbers = [];
+    var userNumbers = [];
+
+    $("#extractbutton").on("click", function (evt1) {
+        var ct = 0;
         evt1.preventDefault();
         $("#messageDiv").html("");
         $("#randomNumbers").html("");
-        for(let i=0;i<6;i++){
-            var number=Math.floor(Math.random() * 49) + 1;
-            while(randomNumbers.includes(number)){
-                number=Math.floor(Math.random() * 49) + 1;
+        for (let i = 0; i < 6; i++) {
+            var number = Math.floor(Math.random() * 49) + 1;
+            while (randomNumbers.includes(number)) {
+                number = Math.floor(Math.random() * 49) + 1;
             }
             randomNumbers.push(number);
-            let rdNumbers=`
+            let rdNumbers = `
             <span>${number}</span>`;
             $("#randomNumbers").append(rdNumbers);
-            if(userNumbers.includes(randomNumbers[i])){
+            if (userNumbers.includes(randomNumbers[i])) {
                 ct++;
             }
         }
-        if(ct===0){
-            let failureMessage=`
+        if (ct === 0) {
+            let failureMessage = `
             <div class="alert alert-danger" role="alert">
             No numbers were guessed!
             </div>`;
             $("#messageDiv").html(failureMessage);
         }
-        else if(ct>0 && ct<6){
-            let winMessage=`
+        else if (ct > 0 && ct < 6) {
+            let winMessage = `
             <div class="alert alert-warning" role="alert">
             You guessed ${ct} numbers!!
             </div>`;
             $("#messageDiv").html(winMessage);
         }
-        else{
-            let successMessage=`
+        else {
+            let successMessage = `
             <div class="alert alert-success" role="alert">
             You guessed all numbers!!
             </div>`;
@@ -43,73 +44,48 @@ $("document").ready(function () {
         }
     });
 
-    $("#submitbutton").on("click", function (evt){
+    $("#submitbutton").on("click", function (evt) {
         evt.preventDefault();
-        let isIncorrect=false;
-        $("#messageDiv").html("");
-        $(".red").removeClass("red");
-
-        var userInput1 = $("#guessInput1").val();
-        var userNumber1 = Number(userInput1);
-        userNumbers.push(userNumber1);
-
-        var userInput2 = $("#guessInput2").val();
-        var userNumber2 = Number(userInput2);
-        userNumbers.push(userNumber2);
-
-        var userInput3 = $("#guessInput3").val();
-        var userNumber3 = Number(userInput3);
-        userNumbers.push(userNumber3);
-
-        var userInput4 = $("#guessInput4").val();
-        var userNumber4 = Number(userInput4);
-        userNumbers.push(userNumber4);
-
-        var userInput5 = $("#guessInput5").val();
-        var userNumber5 = Number(userInput5);
-        userNumbers.push(userNumber5);
-
-        var userInput6 = $("#guessInput6").val();
-        var userNumber6 = Number(userInput6);
-        userNumbers.push(userNumber6);
-
-        if (!(userNumber1>=1 && userNumber1<=49)){
-            isIncorrect=true;
-            $("#guessInput1").addClass("red");
-        }
-
-        if (!(userNumber2>=1 && userNumber2<=49)){
-            isIncorrect=true;
-            $("#guessInput2").addClass("red");
-        }
-
-        if (!(userNumber3>=1 && userNumber3<=49)){
-            isIncorrect=true;
-            $("#guessInput3").addClass("red");
-        }
-
-        if (!(userNumber4>=1 && userNumber4<=49)){
-            isIncorrect=true;
-            $("#guessInput4").addClass("red");
-        }
-
-        if (!(userNumber5>=1 && userNumber5<=49)){
-            isIncorrect=true;
-            $("#guessInput5").addClass("red");
-        }
-
-        if (!(userNumber6>=1 && userNumber6<=49)){
-            isIncorrect=true;
-            $("#guessInput6").addClass("red");
-        }
-        if(isIncorrect===true){
+        let selectedCells = $("#lotoTicket span.selected");
+        if (selectedCells.length < 6) {
             let errorMessage = `
             <div class="alert alert-danger" role="alert">
-            The number must be between 1 and 49!!
+            You must select 6 numbers!!
             </div>`;
             $("#messageDiv").html(errorMessage)
         }
-    })
-        
+        else {
+            userNumbers = [];
+            for (let i = 0; i < 6; i++) {
+                userNumbers.push(Number(selectedCells[i].innerHTML));
+            }
+            $("#extractbutton").css("visibility", "visible");
+        }
 
+    })
+
+    function createGrid(x) {
+        for (var i = 0; i < x; i++) {
+            $("#lotoTicket").append(`<div id='row-${i}'></div>`);
+            for (var j = 0; j < x; j++) {
+                let id = "row-" + i;
+                let number = i * x + j + 1;
+                $("#" + id).append(`<span>${number}</span>`);
+            }
+        }
+
+        $("#lotoTicket span").on("click", function () {
+            let selectedCells = $("#lotoTicket span.selected");
+            if (selectedCells.length < 6) {
+                this.classList.add("selected");
+            }
+        })
+    }
+
+    $("#clearbutton").on("click", function () {
+        $("#lotoTicket span.selected").removeClass("selected");
+        $("#extractbutton").css("visibility", "hidden");
+        $("#messageDiv").html("");
+        $("#randomNumbers").html("");
+    })
 })
